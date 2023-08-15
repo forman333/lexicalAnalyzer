7790ce71c69622e5472cb12089de34a5f6116354
unknown <261963300@formanite.fccollege.edu.pk>
2023-08-15 14:39:29 -0700

    All done with Preprocessorclass, import and block comments handled better now

--- Preprocessor.java
+++ Preprocessor.java
@@ -1,5 +1,6 @@
 package Formality;
 import java.io.*;
+import java.nio.Buffer;
 public class Preprocessor {
 
 	public static void main(String[] args) throws IOException {
@@ -13,39 +14,48 @@ public class Preprocessor {
 			//using buffered reader/writer for file handling
 			try {
 				BufferedReader f = new BufferedReader(new FileReader(input_file));
-				BufferedWriter o = new BufferedWriter(new FileWriter("out1.txt"));
-				String line;
+				BufferedWriter output = new BufferedWriter(new FileWriter("out1.txt"));
+				String line = f.readLine();
 				//operational loop
-				while ((line = f.readLine()) != null) {
+				do {
 					
-					line = annotation(line);
+					line = annotation(line);		//skips line with annotation
 					line = undesiredSpaces(line);	//trims line to eliminate blanksapces
-					line = noImport(line);			//eliminates everything from import till ;
 					line = singleLineComments(line);//eliminates single line comments
-					if(isBlockComment(line)) {		//checks if block comment consisting over more than 1 line, if it is then skips till end of block is reached
-						while(!line.contains("*/")&&line!=null) {
-							line = f.readLine();
-							if(line.contains("*/")) {
-								int indexOfInit = line.indexOf("*/");
-								line = line.substring(indexOfInit);
-								if(line.equals("*/")){
-									line = " ";
-									break;
-								}
-							}
-						}
+					if(isBlockComment(line)) {
+						line = blockComments(line,f);
 					}
-					
-					
+					/*
+					 * if(line.contains("import ")&&(!line.substring(line.indexOf("import ")).
+					 * contains(";"))) { if(line.equals("import"))
+					 * 
+					 * 
+					 * if(line.contains(";")) { int indexOfImport = line.indexOf("import "); int
+					 * indexOfSemi = line.indexOf(";", indexOfImport);
+					 * 
+					 * 
+					 * if (indexOfImport != -1 && indexOfSemi != -1) { line = line.substring(0,
+					 * indexOfImport) + line.substring(indexOfSemi + 1); } } else {
+					 * while(!line.contains(";")&&line!=null) { line = f.readLine(); int
+					 * indexOfImport = line.indexOf("import "); int indexOfSemi = line.indexOf(";",
+					 * indexOfImport);
+					 * 
+					 * 
+					 * if (indexOfImport != -1 && indexOfSemi != -1) { line = line.substring(0,
+					 * indexOfImport) + line.substring(indexOfSemi + 1);
+					 * 
+					 * } } } }
+					 */
+					line = importOut(line,f);	
 					if( blankLines(line)) {			//if theres a blank line, skips the current iteration of line hence skips the blank line
 						continue;
 					}
 					
 					System.out.println(line);
-				}
+				}while ((line = f.readLine()) != null);
 				
 				f.close();
-				o.close();
+				output.close();
 				
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -72,7 +82,6 @@ public class Preprocessor {
 	// Method: eliminate import statement
 			public static String noImport(String line) {
 				if (line.contains("import")){
-					
 					int indexOfImport = line.indexOf("import ");
 			        int indexOfSemi = line.indexOf(";", indexOfImport);
 			        
@@ -88,15 +97,15 @@ public class Preprocessor {
 					return line;
 				}				
 			}
-	// Method eliminate annotations
+	// Method: eliminates annotations
 			public static String annotation(String line) {
 				 if(line.contains("@")) {
 					 int indexOfAnnot = line.indexOf("@");
+					 if(line.equals(line)) {
 					 return line.substring(0,indexOfAnnot)+line.substring(indexOfAnnot+1); 
+					 }
 				 }
-				 else {
-					 return line;
-				 }
+				 return line;
 			}
 	//Method: eliminate comments
 			public static boolean isBlockComment(String line) {
@@ -117,7 +126,70 @@ public class Preprocessor {
 					return line;
 				}
 			}
-			
+	//Method: checks if same Line import statement
+			public static String sameLineImport(String line) {
+				if(line.contains("import ")&&line.contains(";")) {
+					 line = line.substring(0,line.indexOf("import "))+line.substring(line.indexOf(";"));		
+					 if(line.equals(";")) {																		//to avoid null error
+						 return line = " ";
+					 }
+					 else {
+						 return line.substring(0,line.indexOf("import "))+line.substring(line.indexOf(";")+1);
+					 }
+				}
+				else {
+					return line;
+				}
+			}
+	//Method: handles import statement on multiple lines
+			public static String importOut(String line, BufferedReader f) throws IOException {
+				if(line.contains("import ")&&line.contains(";")) {
+					 line = line.substring(0,line.indexOf("import "))+line.substring(line.indexOf(";"));		
+					 if(line.equals(";")) {																		//to avoid null error
+						 return line = " ";
+					 }
+					 else {
+						 return line.substring(0,line.indexOf("import "))+line.substring(line.indexOf(";")+1);
+					 }
+				}
+				
+				else if((line.contains("import ")||line.contains("import"))&&(!line.contains(";"))) {
+					if(line.equals("import")||line.equals("import ")) {
+						while(!line.contains(";")&&f.readLine()!=null) {
+							line = f.readLine();
+					        if (line.contains(";")&&!line.equals(";")) {
+					            line = line.substring(line.indexOf(";") + 1);
+					            return line;
+					        }
+					        else if(line.equals(";")){
+					        	line = f.readLine();
+					        	return line;
+					        }
+							
+						}
+					}	
+				}
+				return line;
+			}
+	//Method: handles mutiple lines block comments
+		
+			public static String blockComments(String line, BufferedReader f) throws IOException {
+				while(!line.contains("*/")&&f.readLine()!=null) {
+					line = f.readLine();
+					if(line.contains("*/")) {
+						int indexOfInit = line.indexOf("*/");
+						line = line.substring(indexOfInit);
+						if(line.equals("*/")){
+							return line = " ";
+							
+						}
+						else {
+							return line.substring(indexOfInit);
+						}
+					}
+				}
+				return line;
+			}
 			
 			
 }
